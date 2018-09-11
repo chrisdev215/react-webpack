3b44c3d52319d7fa3887a346c60bf778c261442a
Cristian Cano <ccano@tecmovin.com>
2018-09-11 00:19:14 -0500

    Se hizo un hola mundo con react

--- src/main.js
+++ src/main.js
@@ -1,1 +1,6 @@
-console.log("Hello World -->")
+import React from 'react';
+import { render } from 'react-dom'
+
+const app = document.getElementById('app')
+
+render(<h1>React y Webpack ( Cristian Cano )</h1>, app)
